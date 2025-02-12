59adb16ed9b6af253fc62c02f228f3d886c2c0b1
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-12 23:21:43 +0100

    refactor: update page header

--- src/shared/components/logo-header.tsx
+++ src/shared/components/logo-header.tsx
@@ -1,34 +1,89 @@
+"use client";
+
 // External Imports
 import Image from "next/image";
 import Link from "next/link";
-import { HTMLAttributes, Ref } from "react";
+import { HTMLAttributes, Ref, useState } from "react";
 
 // Local Imports
+import { Container } from "@/shared/components/container";
+import { TbLogout, TbSettings } from "react-icons/tb";
 import { cn } from "../lib/utils/cn";
+import { Avatar, AvatarFallback } from "./avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
 
 interface LogoHeaderProps extends HTMLAttributes<HTMLDivElement> {
   ref?: Ref<HTMLDivElement>;
 }
 
-export const LogoHeader = ({
-  className,
-  children,
-  ref,
-  ...props
-}: LogoHeaderProps) => {
-  return (
-    <header
-      ref={ref}
-      className={cn("sticky top-0 z-50 flex py-3 border", className)}
-      {...props}
-    >
-      <Link href={"/"}>
-        <div className="relative size-8 md:size-10">
-          <Image src={"/images/logo.png"} alt="Logo" fill />
-        </div>
-      </Link>
+export const LogoHeader = ({ className, ref, ...props }: LogoHeaderProps) => {
+  const [userMenuOpen, setUserMenuOpen] = useState(false);
 
-      {children}
-    </header>
+  return (
+    <div className="sticky top-0 z-100 flex border border-neutral-200 shadow">
+      <Container
+        ref={ref}
+        className={cn("flex items-center justify-between py-2", className)}
+        {...props}
+      >
+        <Link href={"/"}>
+          <div className="relative size-8 md:size-9">
+            <Image src={"/images/logo.png"} alt="Logo" fill />
+          </div>
+        </Link>
+
+        <Popover
+          open={userMenuOpen}
+          onOpenChange={() => setUserMenuOpen((o) => !o)}
+        >
+          <PopoverTrigger asChild>
+            <Avatar className="cursor-default">
+              <AvatarFallback>SA</AvatarFallback>
+            </Avatar>
+          </PopoverTrigger>
+
+          <PopoverContent
+            // sideOffset={12}
+            className="flex flex-col gap-3 p-3 text-sm md:text-base"
+          >
+            {/* Popover Header */}
+            <p className="text-xs text-neutral-500 smd:text-sm">Super Admin</p>
+
+            <div className="flex flex-col gap-4 md:gap-4">
+              {/* User Details */}
+              <div className="flex items-center gap-3">
+                <Avatar className="size-10 md:size-12">
+                  <AvatarFallback>SA</AvatarFallback>
+                </Avatar>
+
+                <div className="-space-y-0.5 md:-space-y-1">
+                  <p className="text-sm font-semibold text-neutral-800 md:text-base">
+                    Super Admin
+                  </p>
+                  <p className="text-xs text-neutral-600 md:text-sm">
+                    superadmin@email.com
+                  </p>
+                </div>
+              </div>
+
+              {/* Options */}
+              <div className="flex flex-col text-sm text-neutral-800 md:text-base">
+                {/* Account */}
+                <div className="flex cursor-pointer items-center gap-2 rounded-xl px-2 py-2 transition md:px-4 md:py-2.5 lg:hover:bg-neutral-300 lg:hover:text-black lg:hover:shadow">
+                  <TbSettings size={24} />
+                  <span>Manage Account</span>
+                </div>
+
+                {/* Sign out */}
+                <div className="flex cursor-pointer items-center gap-2 rounded-xl px-2 py-2 transition md:px-4 md:py-2.5 lg:hover:bg-neutral-300 lg:hover:text-black lg:hover:shadow">
+                  <TbLogout size={24} />
+                  <span>Sign Out</span>
+                </div>
+              </div>
+            </div>
+          </PopoverContent>
+        </Popover>
+      </Container>
+    </div>
   );
 };
