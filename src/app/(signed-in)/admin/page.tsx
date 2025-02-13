f397a1da2991fb9e0250dac32844f97f1cdc5f05
Chiagoziem Sunday <gozmansunday@gmail.com>
2025-02-13 23:17:29 +0100

    fix: resolve missing suspense boundary error

--- src/app/(signed-in)/admin/page.tsx
+++ src/app/(signed-in)/admin/page.tsx
@@ -1,5 +1,6 @@
 // External Imports
 import { TbPlus } from "react-icons/tb";
+import { Suspense } from "react";
 
 // Local Imports
 import { ProductTable } from "@/features/admin/components/product-table";
@@ -9,40 +10,42 @@ import { Separator } from "@/shared/components/separator";
 
 const AdminPage = () => {
   return (
-    <div className="flex flex-1 flex-col gap-10 py-8 md:gap-12 md:py-12">
-      {/* First Part */}
-      <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between md:gap-12">
-        <div>
-          <h1 className="text-3xl font-bold md:text-4xl">
-            Products ({products.length})
-          </h1>
-          <p className="text-sm font-semibold text-neutral-400 md:text-base">
-            Manage products for the store
-          </p>
+    <Suspense>
+      <div className="flex flex-1 flex-col gap-10 py-8 md:gap-12 md:py-12">
+        {/* First Part */}
+        <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between md:gap-12">
+          <div>
+            <h1 className="text-3xl font-bold md:text-4xl">
+              Products ({products.length})
+            </h1>
+            <p className="text-sm font-semibold text-neutral-400 md:text-base">
+              Manage products for the store
+            </p>
+          </div>
+
+          <div>
+            <>
+              {/* Large */}
+              <Button size={"xl"} className="hidden w-full md:inline-flex">
+                <TbPlus size={20} strokeWidth={3} />
+                <span>Add New</span>
+              </Button>
+
+              {/* Small */}
+              <Button size={"lg"} className="w-full md:hidden">
+                <TbPlus size={20} strokeWidth={3} />
+                <span>Add New</span>
+              </Button>
+            </>
+          </div>
         </div>
 
-        <div>
-          <>
-            {/* Large */}
-            <Button size={"xl"} className="hidden w-full md:inline-flex">
-              <TbPlus size={20} strokeWidth={3} />
-              <span>Add New</span>
-            </Button>
+        <Separator />
 
-            {/* Small */}
-            <Button size={"lg"} className="w-full md:hidden">
-              <TbPlus size={20} strokeWidth={3} />
-              <span>Add New</span>
-            </Button>
-          </>
-        </div>
+        {/* Products Table */}
+        <ProductTable />
       </div>
-
-      <Separator />
-
-      {/* Products Table */}
-      <ProductTable />
-    </div>
+    </Suspense>
   );
 };
 
--- src/app/(signed-in)/super-admin/page.tsx
+++ src/app/(signed-in)/super-admin/page.tsx
@@ -1,4 +1,5 @@
 // External Imports
+import { Suspense } from "react";
 import { TbPlus } from "react-icons/tb";
 
 // Local Imports
@@ -9,40 +10,42 @@ import { Separator } from "@/shared/components/separator";
 
 const SuperAdminPage = () => {
   return (
-    <div className="flex flex-1 flex-col gap-10 py-8 md:gap-12 md:py-12">
-      {/* First Part */}
-      <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between md:gap-12">
-        <div>
-          <h1 className="text-3xl font-bold md:text-4xl">
-            Admins ({admins.length})
-          </h1>
-          <p className="text-sm font-semibold text-neutral-400 md:text-base">
-            Manage admins for your store
-          </p>
+    <Suspense>
+      <div className="flex flex-1 flex-col gap-10 py-8 md:gap-12 md:py-12">
+        {/* First Part */}
+        <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between md:gap-12">
+          <div>
+            <h1 className="text-3xl font-bold md:text-4xl">
+              Admins ({admins.length})
+            </h1>
+            <p className="text-sm font-semibold text-neutral-400 md:text-base">
+              Manage admins for your store
+            </p>
+          </div>
+
+          <div>
+            <>
+              {/* Large */}
+              <Button size={"xl"} className="hidden w-full md:inline-flex">
+                <TbPlus size={20} strokeWidth={3} />
+                <span>Add New</span>
+              </Button>
+
+              {/* Small */}
+              <Button size={"lg"} className="w-full md:hidden">
+                <TbPlus size={20} strokeWidth={3} />
+                <span>Add New</span>
+              </Button>
+            </>
+          </div>
         </div>
 
-        <div>
-          <>
-            {/* Large */}
-            <Button size={"xl"} className="hidden w-full md:inline-flex">
-              <TbPlus size={20} strokeWidth={3} />
-              <span>Add New</span>
-            </Button>
+        <Separator />
 
-            {/* Small */}
-            <Button size={"lg"} className="w-full md:hidden">
-              <TbPlus size={20} strokeWidth={3} />
-              <span>Add New</span>
-            </Button>
-          </>
-        </div>
+        {/* Admins Table */}
+        <AdminTable />
       </div>
-
-      <Separator />
-
-      {/* Admins Table */}
-      <AdminTable />
-    </div>
+    </Suspense>
   );
 };
 
